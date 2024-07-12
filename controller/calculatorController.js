f6af030e23c25e4733d019268a0c582fb038c5a4
alkhan097 <alkhan097@hotmail.com>
2024-07-12 08:09:54 -0400

    fifth

--- controller/calculatorController.js
+++ controller/calculatorController.js
@@ -13,7 +13,7 @@ exports.calculate = (req, res) => {
             case 'subtract':
                 result = number1 - number2;
                 break;
-            case  numer1===null:
+            case  numer1 || number2===null:
                 return res.status(400).json({ error: 'Invalid null number' });
             default:
                 return res.status(400).json({ error: 'Invalid operation' });
